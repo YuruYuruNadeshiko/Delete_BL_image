a34a7c96a074c05210238022567aae926d55f957
YuruYuruLife <scudtw07@gmail.com>
2026-06-27 23:20:42 +0900

    Update find_list.py

--- find_list.py
+++ find_list.py
@@ -1,6 +1,6 @@
 from pywinauto import Desktop
 
-win = Desktop(backend="uia").window(title="은빈")
+win = Desktop(backend="uia").window(title="# 여기다 이름 입력")
 lst = win.child_window(auto_id="100", control_type="Pane")
 rect = lst.rectangle()
 print("대화목록 영역:", rect)
